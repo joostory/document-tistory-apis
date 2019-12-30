36077570a047f399e035077778d06f0dec2cde65
Joo <joo@joostory.net>
2019-12-30 15:02:22 +0900

    docz 적용

---
+++ doczrc.js
@@ -0,0 +1,19 @@
+export default {
+  title: '티스토리 Open API',
+  base: './docs',
+  menu: [
+    '기본',
+    {
+      name: '인증 가이드'
+    },
+    {
+      name: 'API 가이드',
+      menu: [
+        '블로그',
+        '카테고리 API',
+        '글 API',
+        '댓글 API',
+      ]
+    }
+  ]
+}
